6c7f9eb76b797e68743899e5e2664c984bf40894
DeYing Zhang <legendzdy@dingtalk.com>
2024-07-15 21:22:26 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import WebRTest from './components/WebRTest.vue'
+</script>
+
+<template>
+  <div id="app">
+    <WebRTest />
+  </div>
+</template>
+
+<style scoped>
+</style>
